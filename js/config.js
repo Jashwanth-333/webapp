765b651e71f670bfa7ef152e543117c2ab2b1b74
Jashwanth-333 <157295184+Jashwanth-333@users.noreply.github.com>
2024-11-17 15:42:51 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_U8ggywKoX
-', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3508tjqs9iuk8vmpnargub2d4n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_ON9YDcUFn', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '35ffodga19uvhfa74ggu85n7g22', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
